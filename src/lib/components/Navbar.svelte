eac7812e7dfdf3d90683d0564054c4235b3574ec
Sean Roche <sean64roche@gmail.com>
2025-04-07 16:58:52 +0100

    tidied up  Navbar.svelte

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,43 +1,70 @@
 <script lang="ts">
+	import { ChevronDown, Menu, Search } from 'lucide-svelte';
 
 	const home = '/';
 	const tournaments = '/tournaments';
 	const formats = '/formats';
 	const players = '/players';
 	const media = '/media';
-
 </script>
+
+{#snippet items()}
+	<li><a href={home}>Home</a></li>
+	<li>
+		<div class="block lg:hidden">
+			<p class="font-bold">Tournaments</p>
+			<ul class="pl-4 bg-base-100 p-2 list-disc">
+				<li><a href={tournaments}>All Tournaments</a></li>
+				<li><a href="{tournaments}/ongoing">Ongoing</a></li>
+				<li><a href="{tournaments}/upcoming">Upcoming</a></li>
+				<li><a href="{tournaments}/completed">Completed</a></li>
+			</ul>
+		</div>
+		<div class="dropdown dropdown-center dropdown-hover hidden lg:block">
+			<button tabindex="0" class="inline-flex items-center gap-1">
+				Tournaments <ChevronDown class="w-4 h-4" />
+			</button>
+			<ul tabindex="-1" class="dropdown-content menu bg-base-100 p-2 list-disc">
+				<li><a href={tournaments}>All Tournaments</a></li>
+				<li><a href="{tournaments}/ongoing">Ongoing</a></li>
+				<li><a href="{tournaments}/upcoming">Upcoming</a></li>
+				<li><a href="{tournaments}/completed">Completed</a></li>
+			</ul>
+		</div>
+	</li>
+	<li><a href={formats}>Formats</a></li>
+	<li><a href={players}>Players</a></li>
+	<li><a href={media}>Media</a></li>
+{/snippet}
 
 <div class="navbar bg-base-100 shadow-sm">
 	<div class="navbar-start">
 		<div class="dropdown">
-			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-			</div>
-			<a href="about" class="btn btn-ghost text-xl">Full Restore Tournaments</a>
-		</div>
-	</div>
-	<div class="navbar-center hidden lg:flex">
-		<ul class="menu menu-horizontal px-1">
-			<li><a href={home}>Home</a></li>
-			<li>
-				<details>
-					<summary>Tournaments</summary>
-					<ul class="p-2">
-						<li><a href={tournaments}>All Tournaments</a></li>
-						<li><a href="{tournaments}/ongoing">Ongoing</a></li>
-						<li><a href="{tournaments}/upcoming">Upcoming</a></li>
-						<li><a href="{tournaments}/completed">Completed</a></li>
+			<div class="navbar bg-base-100 shadow-sm">
+				<details class="dropdown">
+					<summary class="btn btn-ghost btn-circle lg:hidden m-1">
+						<Menu />
+					</summary>
+					<ul class="dropdown-content menu bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+						{@render items()}
 					</ul>
 				</details>
-			</li>
-			<li><a href={formats}>Formats</a></li>
-			<li><a href={players}>Players</a></li>
-			<li><a href={media}>Media</a></li>
+				<a href="/" class="btn btn-ghost text-xl">Full Restore Tournaments</a>
+			</div>
+		</div>
+	</div>
+	<div class="dropdown">
+
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="dropdown-content menu menu-horizontal px-1">
+			{@render items()}
 		</ul>
 	</div>
-	<div class="navbar-end">
-		<input type="text" placeholder="Search..." class="input input-bordered w-24 md:w-auto" />
-<!--		<input type="checkbox" value="synthwave" class="toggle theme-controller" />-->
+	<div class="navbar-end hidden md:flex lg:flex">
+		<label class="input">
+			<Search class="w-4 h-4" />
+			<input type="search" class="grow" required placeholder="Search" />
+		</label>
 	</div>
 </div>

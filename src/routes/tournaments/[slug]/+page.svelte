3511058a8871aee9dfc32bde63a0557546783ab1
Sean Roche <sean64roche@gmail.com>
2025-04-10 20:24:24 +0100

    more placeholders and services pkg

---
+++ src/routes/tournaments/[slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { PageProps } from './$types';
+
+let { data }: PageProps = $props();
+</script>
+
+<div class="container mx-auto py-8 px-4">
+	<div class="mb-8">
+		<h1 class="text-4xl font-bold mb-2">{data.title}</h1>
+		<p class="text-gray-500">
+			{data.content}
+		</p>
+	</div>
+	<div class="space">
+	</div>
+</div>

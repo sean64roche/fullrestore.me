e821a5c734684399b51b43d0178faf51f29180b7
Sean Roche <sean64roche@gmail.com>
2025-05-16 17:38:32 +0100

    move RoundList.svelte to own component

---
+++ src/lib/components/round/RoundList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Ellipsis } from 'lucide-svelte';
+	import type { RoundQParams } from '../../../api/roundsApi';
+  import type { TournamentQParams } from '../../../api/tournamentsApi';
+
+	let { tournament, rounds, status }: {
+		tournament: TournamentQParams,
+		rounds: RoundQParams[],
+		status: string
+	} = $props();
+	let visibleRounds = (rounds.slice(0, 4));
+	let hiddenRounds = (rounds.length > 4 ? rounds.slice(4) : []);
+</script>
+
+<div class="mt-2 md:mt-0 md:static">
+	<div class="flex gap-2 items-center justify-end">
+		{#each visibleRounds as round}
+			<a href="/tournament/{tournament.slug}/r{round.toString()}" class="link text-sm">
+				{round === rounds[0] && status === 'completed' ? 'Finals' : `Round ${round}`}
+			</a>
+		{/each}
+		{#if hiddenRounds.length > 0}
+			<div class="dropdown dropdown-end dropdown-hover">
+				<button
+					tabindex="0"
+					class="btn btn-ghost btn-xs"
+				>
+					<Ellipsis class="w-4 h-4" />
+				</button>
+				<ul class="dropdown-content absolute right-0 top-6 bg-base-100 shadow-lg rounded-lg p-2 z-10 w-36 flex flex-col gap-1">
+					{#each hiddenRounds as round}
+						<li><a href="/tournament/{tournament.slug}/r{round.toString()}" class="link text-sm block p-1 hover:bg-base-200 rounded">
+							{`Round ${round}`}
+						</a></li>
+					{/each}
+				</ul>
+			</div>
+		{/if}
+	</div>
+</div>
--- src/lib/components/tournament/TournamentCard.svelte
+++ src/lib/components/tournament/TournamentCard.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import { Calendar, Ellipsis } from 'lucide-svelte';
+	import { Calendar } from 'lucide-svelte';
   import type { TournamentQParams } from '../../../api/tournamentsApi';
   import { compareAsc } from 'date-fns';
+  import RoundList from '$components/round/RoundList.svelte';
 
 	let { tournament }: { tournament: TournamentQParams } = $props();
 	let rounds: number[] = tournament.rounds.
 		map(round => round.roundNumber).
 		toSorted((a, b) => b - a);
 
-	let visibleRounds = (rounds.slice(0, 4));
-	let hiddenRounds = (rounds.length > 4 ? rounds.slice(4) : []);
 	let descExpanded = $state(false);
 	let canExpandDesc = $state(false);
 	let textContainer: HTMLDivElement | undefined;
@@ -81,32 +80,10 @@
 					{@render status()}
 				</div>
 			</div>
-			<div class="mt-2 md:mt-0 md:static">
-				<div class="flex gap-2 items-center justify-end">
-					{#each visibleRounds as round}
-						<a href="/tournament/{tournament.slug}/r{round.toString()}" class="link text-sm">
-							{round === rounds[0] && getStatus(tournament.startDate, tournament.finishDate).status === 'completed' ? 'Finals' : `Round ${round}`}
-						</a>
-					{/each}
-					{#if hiddenRounds.length > 0}
-						<div class="dropdown dropdown-end dropdown-hover">
-							<button
-								tabindex="0"
-								class="btn btn-ghost btn-xs"
-							>
-								<Ellipsis class="w-4 h-4" />
-							</button>
-							<ul class="dropdown-content absolute right-0 top-6 bg-base-100 shadow-lg rounded-lg p-2 z-10 w-36 flex flex-col gap-1">
-								{#each hiddenRounds as round}
-									<li><a href="/tournament/{tournament.slug}/r{round.toString()}" class="link text-sm block p-1 hover:bg-base-200 rounded">
-										{`Round ${round}`}
-									</a></li>
-								{/each}
-							</ul>
-						</div>
-					{/if}
-				</div>
-			</div>
+			<RoundList tournament={tournament}
+			 rounds={rounds}
+			 status={getStatus(tournament.startDate, tournament.finishDate).status}
+			/>
 		</div>
 		<div
 			bind:this={textContainer}

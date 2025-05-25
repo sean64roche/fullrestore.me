bcb4fd5181fca050d58d1999d563e1aff97fe469
Sean Roche <sean64roche@gmail.com>
2025-05-25 20:26:06 +0100

    add urls to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 		fs: {
 			allow: [],
 		},
+		allowedHosts: ['localhost', '127.0.0.1', 'v2202505274840342452.luckysrv.de', 'fullrestore.me'],
 	},
 	optimizeDeps: {
 		exclude: ['node_modules/.cache'],
